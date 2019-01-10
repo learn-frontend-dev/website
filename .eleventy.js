75e2f5be5cf8b2d8add414b2cdced3f548d3a8fc
Kushagra Gour <chinchang457@gmail.com>
2019-01-10 16:46:37 +0530

    more additions

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,36 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+
+const markdownItConfig = {
+  html: true,
+  breaks: true,
+  linkify: true
+};
+const markdownItAnchorConfig = {
+  permalink: true,
+  permalinkClass: "bookmark",
+  permalinkSymbol: "#"
+};
+
+const markdownLib = markdownIt(markdownItConfig).use(
+  markdownItAnchor,
+  markdownItAnchorConfig
+);
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.setLibrary("md", markdownLib);
   // eleventyConfig.addLayoutAlias("default", "layouts/default.html");
 
   // Create a collection for blog posts only.
   eleventyConfig.addCollection("learnPosts", collection => {
-    return collection.getFilteredByGlob("learn/*.md").map(post => {
-      console.log(99, post.data.title);
-      return post;
-    });
+    return collection.getFilteredByGlob("learn/*.md");
   });
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+
   return {
     pathPrefix: "/website/"
   };
--- about.md
+++ about.md
@@ -1,5 +1,6 @@
 ---
 title: About LearnFrontend.ninja
+layout: default
 ---
 
 Hello! My name is Kushagra Gour. I have been a full-time frontend developer for 7 years and now an indiependent product maker. This website is an attempt to collect and share whatever knowledge I have gained over the past years in the field of Frontend development. Also, I wanted to create a a very easily accessible and consumable starting point for someone who has a wish today to start learning Frontend development.
